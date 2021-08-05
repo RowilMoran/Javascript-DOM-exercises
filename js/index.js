7f7165de90334897b4c168258f067beebfa66002
RowilMoran <rowilmoran@gmail.com>
2021-08-05 18:16:35 -0300

    some final modifications

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ import scrollButton from "./scroll-button.js";
 document.addEventListener("DOMContentLoaded", (e) => {
     menuHambur(".btn", ".panel", ".menu a" );
     scrollButton("scroll-button", "button-hide");
-   darkAndLight(".button-x", "dark-mode"); 
 })
 
+darkAndLight(".button-x", "dark-mode"); 
--- js/light_night.js
+++ js/light_night.js
@@ -1,26 +1,37 @@
 export default function darkAndLight(btn, darkClass) {
     const $button = document.querySelector(btn)
     const $icon = $button.querySelector('.fa-moon')
+    const $dataDark = document.querySelectorAll("[data-dark]")
     
-  let $dataDark = document.querySelectorAll("[data-dark]")
-  console.log($dataDark)
+    const lightMode = () => {
+        $icon.classList.replace("fa-sun", "fa-moon");
+        $dataDark.forEach(element => element.classList.remove(darkClass));
+        localStorage.setItem("theme", "light")
+       
+    }
+
+    const darkMode = () => {
+        $icon.classList.replace("fa-moon", "fa-sun")
+        $dataDark.forEach(element => element.classList.add(darkClass)); 
+        localStorage.setItem("theme", "dark")
+    }
+
 
 
    document.addEventListener("click", (e) => {
        if (e.target.matches(btn) || e.target.matches(`${btn} *`)) {
-           if ($icon.classList.contains("fa-moon")) {
-               $icon.classList.replace("fa-moon", "fa-sun")
-               $dataDark.forEach(element => 
-                   element.classList.add(darkClass)
-               );
-           } else{
-            $icon.classList.replace("fa-sun", "fa-moon");
-            $dataDark.forEach(element => {
-                element.classList.remove(darkClass)
-            });
-           }
+            if ($icon.classList.contains("fa-moon")) {
+               darkMode()
+            } else {
+               lightMode()
+            }
+        } 
+    });
 
-       }
-   }) 
-  
-} 
+    document.addEventListener("DOMContentLoaded", (e) => {
+        if (localStorage.getItem("theme") === null) localStorage.setItem("theme", "light")
+        if (localStorage.getItem("theme") === "light") lightMode();
+        if (localStorage.getItem("theme") === "dark") darkMode();
+
+    })
+}
